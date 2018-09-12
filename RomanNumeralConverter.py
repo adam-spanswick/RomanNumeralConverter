68602f526fd487d5f5d60cf6ae90353a9e3701b6
aspanswick <aspanswick@unm.edu>
2018-09-11 18:48:33 -0600

    Roman Numeral Converter:    - GUI        - Added button for usage message        - Label now updates with correct integer value

--- RomanNumeralConverter.py
+++ RomanNumeralConverter.py
@@ -1,10 +1,11 @@
 # Adam Spanswick
 # This program takes a Roman numeral as a string and converts it to it's corresponding integer value.
+from tkinter import Tk
 from typing import Dict
 from tkinter import *
 from PIL import Image, ImageTk
 
-romanNumeralVals: Dict[str, int] = {
+roman_numeral_values: Dict[str, int] = {
     "I": 1,
     "V": 5,
     "X": 10,
@@ -18,77 +19,109 @@ romanNumeralVals: Dict[str, int] = {
 class RomanNumeralLookup:
 
     @staticmethod
-    def lookup(numeral):
-        if numeral in romanNumeralVals:
-            return romanNumeralVals.get(numeral)
+    def lookup(roman_numeral):
+        if roman_numeral in roman_numeral_values:
+            return roman_numeral_values.get(roman_numeral)
 
-    def convert(self, numeral):
-        number: int = 0
+    def convert(self, roman_numeral):
+        integer_number: int = 0
 
-        if len(numeral) == 1:
-            number = self.lookup(numeral[0])
-            return number
+        if len(roman_numeral) == 1:
+            integer_number = self.lookup(roman_numeral[0])
+            return integer_number
 
-        for c in numeral:
-            number += self.lookup(c)
+        for c in roman_numeral:
+            integer_number += self.lookup(c)
 
-        if numeral[-1] is not "I" and numeral[-2] is "I":
-            number -= 1
+        if roman_numeral[-2] == 'I' and roman_numeral[-1] != 'I':
+            integer_number -= 2
 
-        return number
+        return integer_number
 
 
-window = Tk()
-window.geometry("400x400")
+window: Tk = Tk()
+window.geometry("400x450")
 window.title("Roman Numeral Converter")
-window.configure(background='gray')
+window.configure(background='dark olive green')
 numeral = StringVar()
-integervalue = StringVar()
+integer_value = StringVar()
+
 
 def get_numeral():
-    global inputNumeral
-    romanNumeralToConvert = inputNumeral.get()
-    exitMsg = "exit"
+    global input_numeral, integer_val
+    roman_numeral_to_convert = input_numeral.get()
+    exit_message = "exit"
 
     while True:
-        if romanNumeralToConvert.lower() == exitMsg:
+        if roman_numeral_to_convert.lower() == exit_message:
             break
 
-        individualNumerals = list(romanNumeralToConvert)
-        individualNumerals = [element.upper() for element in individualNumerals]
-        print(individualNumerals)
+        individual_numerals = list(roman_numeral_to_convert)
+        individual_numerals = [element.upper() for element in individual_numerals]
+        print(individual_numerals)
 
-        for char in individualNumerals:
-            if char in romanNumeralVals:
+        if len(individual_numerals) == 0:
+            integer_val = 0
+            return
+
+        for char in individual_numerals:
+            if char in roman_numeral_values:
                 continue
             else:
                 print("Input is not a roman numeral, see usage and re-enter a roman numeral.")
                 break
-        integerval = numLookup.convert(individualNumerals)
-        print(integerval)
+        integer_val = number_lookup.convert(individual_numerals)
+        print(integer_val)
         break
+    font = ('times', 12, 'bold')
+    L4 = Label(window, background="dark olive green", text=integer_val, font=font)
+    L4.grid(row=2, column=1, pady=(15, 15))
+    return integer_val
 
 
-numLookup = RomanNumeralLookup()
+def show_usage():
+    popup = Toplevel(window)
+    popup.configure(background='dark olive green')
+    popup.title("Usage")
+    usage_message = ("This program takes a Roman numeral as a string and converts it to it's corresponding "
+                     "integer value.\n"
+                     "Usage:\n"
+                     "      When prompted enter a roman numeral, capital or lower case. To exit program enter: exit\n"
+                     "Roman Numeral Values:\n"
+                     "      I = 1\n"
+                     "      V = 5\n"
+                     "      X = 10\n"
+                     "      L = 50\n"
+                     "      C = 100\n"
+                     "      D = 500\n"
+                     "      M = 1000\n")
+    # print(usage_message)
+    L3 = Label(popup, background="dark olive green", text=usage_message)
+    L3.grid(row=2, column=1, pady=(15, 15))
+
+
+number_lookup = RomanNumeralLookup()
 
 L1 = Label(window, text="Roman Numeral: ")
-L1.configure(background='gray')
+L1.configure(background='dark olive green')
 L2 = Label(window, text="Integer Value: ")
-L2.configure(background='gray')
-inputNumeral = Entry(window, bd=5, bg="white", textvariable=numeral, relief=FLAT)
-inputNumeral.configure(background='white')
-inputNumeral.focus_set()
-getText = Button(window, text="Submit", relief=FLAT, command=get_numeral)
-integerVal = Label(window, bg="gray", text=integervalue)
+L2.configure(background='dark olive green')
+input_numeral = Entry(window, bd=5, bg="white", textvariable=numeral, relief=FLAT)
+input_numeral.configure(background='white')
+input_numeral.focus_set()
+get_text = Button(window, text="Submit", relief=FLAT, command=get_numeral)
+show_usage = Button(window, text="Usage", relief=FLAT, command=show_usage)
+# integer_label = Label(window, bg="dark olive green", text=display_integer)
 img = ImageTk.PhotoImage(Image.open("romanNumerals.png"))
 pic = Label(window, image=img)
 
-L1.grid(row=0, column=0, pady=(15,15))
-inputNumeral.grid(row=0, column=1, pady=(15,15))
-L2.grid(row=2, column=0, pady=(15,15))
-integerVal.grid(row=2, column=1, pady=(15,15))
-pic.grid(row=6, column=1, pady=(15,15))
-getText.grid(row=4, column=1, pady=(15,15))
+L1.grid(row=0, column=0, pady=(15, 15))
+input_numeral.grid(row=0, column=1, pady=(15, 15))
+L2.grid(row=2, column=0, pady=(15, 15))
+# integer_label.grid(row=2, column=1, pady=(15, 15))
+pic.grid(row=6, column=1, pady=(15, 15))
+get_text.grid(row=3, column=1, pady=(15, 15))
+show_usage.grid(row=4, column=1, pady=(15, 15))
 
 window.mainloop()
 
